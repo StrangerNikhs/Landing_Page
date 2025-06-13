f3cd3f5ae1349e7f87d8b90ef817cdafa64a390e
StrangerNikhs <138905829+StrangerNikhs@users.noreply.github.com>
2025-06-13 13:39:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
